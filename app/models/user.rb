5c8ead0dc27f8f1f64deba85a74b67a05f052da6
Gordon Tinyefuza <gordon.tinyefuza@gmail.com>
2022-03-08 13:35:09 +0300

    Add email confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :inventories
   has_many :foods
   has_many :recipes
-
-  # def self.recent_post(id)
-  #   Post.where(user_id: id).limit(3)
-  # end
-
-  # def self.all_post(id)
-  #   Post.where(user_id: id)
-  # end
 end
