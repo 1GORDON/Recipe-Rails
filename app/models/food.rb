1a4ea86d7e2c6b1fcdb6b1f2c20f3809593b8a48
Kingstalux <kingsleyngu25@gmail.com>
2022-03-09 07:18:53 +0100

    Add foods validation

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,4 +2,7 @@ class Food < ApplicationRecord
   belongs_to :user
   has_many :inventoryFoods
   has_many :recipeFoods
+  validates :name, presence: true
+  validates :measurement_unit, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 1 }
 end
