06229b6afac1fcec629a67da06a9d154099a5cca
Kingstalux <kingsleyngu25@gmail.com>
2022-03-09 18:08:35 +0100

    Add validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,8 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipeFoods
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :cooking_time, presence: true
+  validates :preperation_time, presence: true
 end
