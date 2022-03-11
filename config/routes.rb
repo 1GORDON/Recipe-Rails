cb1698373396d448b7b6e55cd8887a4a142f57f3
Kingstalux <kingsleyngu25@gmail.com>
2022-03-11 18:59:18 +0100

    Add user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
+
   devise_scope :user do
+    authenticated  do
+      root to: 'foods#index'
+   end
+
+  unauthenticated do
+        root to: 'devise/sessions#new', as: 'unauthenticated_root'
+  end
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  root "foods#index"
   resources :foods, only: [:index, :new, :create, :destroy]
   resources :recipes, only: [:index, :new, :create, :destroy, :show] do
     resources :recipe_foods, only: [:new, :edit, :create, :destroy, :update]
