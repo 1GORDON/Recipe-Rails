8b9065a9f2cbad95cc009528d3f7d647b5470b2f
Kingstalux <kingsleyngu25@gmail.com>
2022-03-11 16:11:00 +0100

    Edit food model tests

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-   user1 = User.new(name: 'Tom', email: 'gbuldie11@gmail.com', password: 'godanah12112', confirmed_at: Time.now)
 
-  subject { Food.create! name: 'Milk', measurement_unit: 'grams', price: '10', user: user1 }
-
-  before { subject.save }
+  before(:each) do
+    @user1 = User.create! name: 'Justin', password: '000000', email: 'justin@gmail.com',
+                          confirmed_at: Time.now
+    subject { Food.create(name: 'Milk', measurement_unit: 'grams', price: '10', user: user1) }
+    subject.save
+  end
 
   it 'name should be present' do
     subject.name = nil
@@ -13,7 +15,8 @@ RSpec.describe Food, type: :model do
   end
 
   it 'Measurement unit should be present' do
-    expect(subject).to be_valid
+    subject.measurement_unit = nil
+    expect(subject).to_not be_valid
   end
 
   it 'Price should be present' do
