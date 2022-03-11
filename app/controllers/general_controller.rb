8f381202be7798e9392e0ac731ef11569880bd3d
Gordon Tinyefuza <gordon.tinyefuza@gmail.com>
2022-03-11 14:06:31 +0300

    Add total price for missing recipes

--- app/controllers/general_controller.rb
+++ app/controllers/general_controller.rb
@@ -5,5 +5,7 @@ class GeneralController < ActionController::Base
     @recipe = Recipe.where(user_id: @user.id)
     @recipe_foods = RecipeFood.all
     @foods = Food.where(user_id: @user.id)
+    @recipes = @user.recipes
+    @total = @recipes.map { |x| Recipe.total_value(x.id) }.reduce(:+)
   end
 end
