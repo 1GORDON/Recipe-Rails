fc1707412a854bae26562b089598e2dd1b43ff99
Kingstalux <kingsleyngu25@gmail.com>
2022-03-11 12:36:17 +0100

    Add spec folder

--- Gemfile
+++ Gemfile
@@ -64,6 +64,7 @@ gem 'bootsnap', require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
+  gem 'rspec-rails'
   gem 'capybara'
   gem 'database_cleaner'
   gem 'selenium-webdriver'
@@ -71,7 +72,7 @@ group :development, :test do
 end
 
 group :test do
-  gem 'rspec-rails'
+  # gem 'rspec-rails'
 end
 
 group :development do
--- app/controllers/general_controller.rb
+++ app/controllers/general_controller.rb
@@ -1,11 +1,17 @@
 class GeneralController < ActionController::Base
   def index
-    # @foods = Food.includes(:recipeFoods).all.where("user_id != ?", current_user.id)
     @user = current_user
     @recipe = Recipe.where(user_id: @user.id)
     @recipe_foods = RecipeFood.all
     @foods = Food.where(user_id: @user.id)
     @recipes = @user.recipes
     @total = @recipes.map { |x| Recipe.total_value(x.id) }.reduce(:+)
+    @food_count = 0
+    @recipes.each do |recipe|
+      recipe_foods = recipe.recipeFoods
+      recipe_foods.each do |_food|
+        @food_count += 1
+      end
+    end
   end
 end
